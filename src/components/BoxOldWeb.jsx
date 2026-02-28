e285ad903e3e9354243f20fb2ce4b9552bab1467
damzproject <nokosvelzz@gmail.com>
2026-02-28 19:03:33 +0700

    damz

--- src/components/BoxOldWeb.jsx
+++ src/components/BoxOldWeb.jsx
@@ -1,7 +1,7 @@
 const Forum_Rahasia_XI_D = () => {
 	return (
 		<div id="Forum_Rahasia_XI_D">
-			<a href="https://base.xtkj3phi.repl.co/">
+			<a href="https://ngl.link/xid_confession_room">
 				<div className="flex justify-between">
 					<img src="/link.png" alt="" className="w-auto h-6" />
 					<img src="/next.png" alt="" className="h-3 w-3" />
